7c54953d9659d2d0d924edc3f3a0c56195ae01dc
Muhammed Taha Kızıkoğlu <tkizikoglu10@gmail.com>
2026-05-26 03:59:52 +0300

    fix: laravel 11 trust proxies fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Middleware\AdminMiddleware;
-use App\Http\Middleware\ActiveUserMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -9,15 +7,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin' => AdminMiddleware::class,
-            'active' => ActiveUserMiddleware::class,
-        ]);
+        
+        // LARAVEL 11 PROXY AYARI: Render üzerindeki HTTPS kilitlenmesini çözer
+        $middleware->trustProxies(at: '*');
+        
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
