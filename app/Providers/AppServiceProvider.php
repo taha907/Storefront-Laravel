28dbfabd82b7f35880499712306badc99601dbb5
Muhammed Taha Kızıkoğlu <tkizikoglu10@gmail.com>
2026-05-26 03:07:04 +0300

    fix: app service provider syntax

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,17 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        \Illuminate\Pagination\Paginator::useBootstrapFive();
+        // Eğer uygulama canlı ortamda (Render'da) çalışıyorsa linkleri HTTPS'e zorla
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
