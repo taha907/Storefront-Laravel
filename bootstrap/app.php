4c7deed7ab1ca63b789fc3778bf0b99b42b4e652
Muhammed Taha Kızıkoğlu <tkizikoglu10@gmail.com>
2026-05-26 22:09:54 +0300

    commit+push değişikliği 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\ForceHttpsMiddleware;
+use App\Http\Middleware\SessionDiagnosticsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,6 +20,10 @@ return Application::configure(basePath: dirname(__DIR__))
         // Render'da bazen ilk istek HTTP gelebiliyor; Secure session cookie yazılmadığı için 419 oluşuyor.
         // Production'da her isteği HTTPS'e zorla.
         $middleware->append(ForceHttpsMiddleware::class);
+
+        // Render'da 419 kök nedenini yakalamak için opsiyonel teşhis.
+        // Render Environment'a SESSION_DIAGNOSTICS=true eklenince log'a fingerprint basar.
+        $middleware->append(SessionDiagnosticsMiddleware::class);
         
     })
     ->withExceptions(function (Exceptions $exceptions) {
